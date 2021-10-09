8519ea3fe764017193e3c575f8df441a4e63ce50
riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>
2021-10-09 23:51:23 +0530

    Minor changes
    Signed-off-by: riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import {NavHashLink} from 'react-router-hash-link';
 const Navbar=()=>{
     return(
         <div className="Navbar">
-            <NavHashLink exact={true} activeClassName='is-active' to="#home">Home</NavHashLink>
+            <NavHashLink smooth exact={true} activeClassName='is-active' to="#home">Home</NavHashLink>
             <NavHashLink smooth exact={true} activeClassName='is-active' to="#about">About</NavHashLink>
-            <NavHashLink exact={true} activeClassName='is-active' to="#rooms">Rooms</NavHashLink>
-            <NavHashLink exact={true} activeClassName='is-active' to="#services">Services</NavHashLink>
-            <NavHashLink exact={true} activeClassName='is-active' to="#gallery">Gallery</NavHashLink>
-            <NavHashLink exact={true} activeClassName='is-active' to="#thingstodo">See & Do</NavHashLink>
-            <NavHashLink exact={true} activeClassName='is-active' to="#contactus">Contact</NavHashLink>
+            <NavHashLink smooth exact={true} activeClassName='is-active' to="#rooms">Rooms</NavHashLink>
+            <NavHashLink smooth exact={true} activeClassName='is-active' to="#services">Services</NavHashLink>
+            <NavHashLink smooth exact={true} activeClassName='is-active' to="#gallery">Gallery</NavHashLink>
+            <NavHashLink smooth exact={true} activeClassName='is-active' to="#thingstodo">See & Do</NavHashLink>
+            <NavHashLink smooth exact={true} activeClassName='is-active' to="#contactus">Contact</NavHashLink>
             <button>Book A Room</button>
             
         </div>
